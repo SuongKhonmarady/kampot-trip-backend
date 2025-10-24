28819b3cb2c414864d95d9a226f03d68fa6a3268
Suong Khonmarady <maradybor@gmail.com>
2025-10-24 11:00:05 +0700

    Fix allowed_origins in CORS configuration to correct domain name

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://derleng.tech', 'https://kampot-trip-front.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://tosderleng.tech', 'https://kampot-trip-front.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
